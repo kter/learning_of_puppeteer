3abbcfc9694e5817f2cf0fef8f20f31668e04e28
kter <me@kter.jp>
2018-10-04 01:34:15 +0900

    とりあえずコミット

---
+++ index.test.js
@@ -0,0 +1,10 @@
+let page
+
+beforeAll(async () => {
+  page = await browser.newPage()
+  await page.goto('https://google.com')
+})
+
+test('should display `google` text on page', async () => {
+  await expect(page).toMatch('google')
+})
---
+++ jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    server: {
+      command: 'node server.js',
+      port: 4444,
+    },
+    launch: {
+        dumpio: true,
+        headless: process.env.HEADLESS !== 'false',
+      },
+      browserContext: 'default',
+      preset: 'jest-puppeteer',
+  }
