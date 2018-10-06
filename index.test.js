1033d119ec3d5b26d8d373532a004fced0a2dce2
kter <me@kter.jp>
2018-10-06 12:16:29 +0900

    ログインができるようになった

--- index.test.js
+++ index.test.js
@@ -1,50 +1,60 @@
-beforeAll(async () => {
-  const page = await browser.newPage()
-  await page.goto('https://google.com')
-})
+const timeout = 5000
 
-test('should display `google` text on page', async () => {
-  await expect(page).toMatch('google')
-})
+describe('Wordpress test', () => {
+  let page
+
+  beforeAll(async () => {
+    page = await browser.newPage()
+    await page.goto('http://localhost/wp-login.php')
+  }, timeout)
+
+  afterAll(async () => {
+    await page.close()
+  })
+
+  it('should display `Username or Email Address` text on login page', async () => {
+    await expect(page).toMatch('Username or Email Address')
+  })
+
+  it('should login', async () => {
+    await page.click('#login > #loginform > p > label > #user_login')
+    await page.type('#login > #loginform > p > label > #user_login', 'testuser')
+
+    await page.click('#login > #loginform > p > label > #user_pass')
+    await page.type('#login > #loginform > p > label > #user_pass', 'testpass')
+
+    await page.click('.login > #login > #loginform > .submit > #wp-submit')
+
+    const navigationPromise = page.waitForNavigation()
+    await navigationPromise
+
+    await expect(page).toMatch('Dashboard')
+  })
+}, timeout)
 
 
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  const navigationPromise = page.waitForNavigation()
-  
-  await page.setViewport({ width: 1527, height: 915 })
-  
-  await page.goto('http://localhost/wp-login.php')
-  
-  await page.waitForSelector('#login > #loginform > p > label > #user_login')
-  await page.click('#login > #loginform > p > label > #user_login')
-  
-  await page.waitForSelector('#login > #loginform > p > label > #user_login')
-  await page.click('#login > #loginform > p > label > #user_login')
-  
-  await page.type('#login > #loginform > p > label > #user_login', 'testuser')
-  
-  await navigationPromise
-  
-  
-  await page.waitForSelector('#adminmenu > #menu-posts > .wp-submenu > li:nth-child(3) > a')
-  await page.click('#adminmenu > #menu-posts > .wp-submenu > li:nth-child(3) > a')
-  
-  await navigationPromise
-  
-  await page.type('#post-body > #post-body-content > #titlediv > #titlewrap > #title', 'title')
-  
-  await page.waitForSelector('.inside > #submitpost > #major-publishing-actions > #publishing-action > #publish')
-  await page.click('.inside > #submitpost > #major-publishing-actions > #publishing-action > #publish')
-  
-  await navigationPromise
-  
-  await page.waitForSelector('#wpbody-content > .wrap > #message > p > a')
-  await page.click('#wpbody-content > .wrap > #message > p > a')
-  
-  await navigationPromise
-  
-  await browser.close()
-})()
+
+
+// const puppeteer = require('puppeteer');
+// (async () => {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   const navigationPromise = page.waitForNavigation()
+//   await page.setViewport({ width: 1527, height: 915 })
+//   
+//   await navigationPromise
+//   
+//   await page.type('#post-body > #post-body-content > #titlediv > #titlewrap > #title', 'title')
+//   
+//   await page.waitForSelector('.inside > #submitpost > #major-publishing-actions > #publishing-action > #publish')
+//   await page.click('.inside > #submitpost > #major-publishing-actions > #publishing-action > #publish')
+//   
+//   await navigationPromise
+//   
+//   await page.waitForSelector('#wpbody-content > .wrap > #message > p > a')
+//   await page.click('#wpbody-content > .wrap > #message > p > a')
+//   
+//   await navigationPromise
+//   
+//   await browser.close()
+// })()
